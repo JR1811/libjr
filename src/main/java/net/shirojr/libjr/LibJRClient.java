f7bba0db8053521652b0aa9e086121ea9ea050c3
JR1811 <janreichelt77@gmail.com>
2024-07-18 18:54:40 +0200

    added new icon and client entrypoint

---
+++ src/main/java/net/shirojr/libjr/LibJRClient.java
@@ -0,0 +1,11 @@
+package net.shirojr.libjr;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LibJRClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
